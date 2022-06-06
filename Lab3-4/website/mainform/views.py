fb2b54af3fe170cd16dcd508e5ef322cbc7fd03c
w105 <kozeev1ilya@gmail.com>
2022-06-06 22:34:44 +0300

    Полная работоспособность базы данных Postgresql

--- Lab3-4/website/mainform/views.py
+++ Lab3-4/website/mainform/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import PlantModel
 
-def index(request):
-    return render(request, 'mainform/index.html')
 
 def about(request):
     return render(request, 'mainform/about.html')
 
+
 def register(request):
     return render(request, 'mainform/register.html')
 
+
 def login(request):
     return render(request, 'mainform/login.html')
+
+
+def index(request):
+    plants = PlantModel.objects.order_by('-id')
+    return render(request, 'mainform/index.html', {'PlantModel': plants})
+
 
